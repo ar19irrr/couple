3801521869ff570966b7a2ea7338a33405e8ffda
ar19irrr <162325792+ar19irrr@users.noreply.github.com>
2026-07-26 19:36:40 +0330

    create_session.py

--- create_session.py
+++ create_session.py
@@ -1,26 +1,16 @@
 import asyncio
 from telethon import TelegramClient
-
-# اطلاعات API رو از config بگیر
-try:
-    import config
-    API_ID = config.API_ID
-    API_HASH = config.API_HASH
-except:
-    API_ID = 2040
-    API_HASH = "b18441a1ff607e10a989891a5462e627"
+import config
 
 async def main():
     print("🔑 در حال ساخت نشست...")
-    
-    client = TelegramClient('session', API_ID, API_HASH)
+    client = TelegramClient('session', config.API_ID, config.API_HASH)
     
     try:
         await client.start()
         me = await client.get_me()
         print(f"✅ نشست با موفقیت ساخته شد!")
         print(f"👤 کاربر: {me.first_name} (ID: {me.id})")
-        
     except Exception as e:
         print(f"❌ خطا: {e}")
     finally:
