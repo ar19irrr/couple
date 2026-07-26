cb2f670cbc52ed78e68813294ce0da28104ea03c
ar19irrr <162325792+ar19irrr@users.noreply.github.com>
2026-07-26 19:36:18 +0330

    config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "توکن_خود_را_اینجا_قرار_دهید")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8858810413:AAGbvBzQ3lQzi_epN47QSmaOh8Ec1hgz0CE")
 API_ID = int(os.environ.get("API_ID", 2040))
 API_HASH = os.environ.get("API_HASH", "b18441a1ff607e10a989891a5462e627")
