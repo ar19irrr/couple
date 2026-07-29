84aa686dfb66200f6666cc0bc84899554dfd31b3
ar19irrr <162325792+ar19irrr@users.noreply.github.com>
2026-07-29 12:39:18 +0330

    bot.py

--- bot.py
+++ bot.py
@@ -695,7 +695,7 @@ def main():
     schedule_monthly_announcement(dp)
     
     logger.info("🚀 ربات شروع به کار کرد...")
-    updater.start_polling(drop_pending_updates=True)
+    updater.start_polling(drop_pending_updates=True, timeout=30)
     updater.idle()
 
 if __name__ == "__main__":
